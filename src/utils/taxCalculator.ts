daf17c777c34cc18c4b0916b14313663a4f631aa
4drea4 <bakerand@outlook.com>
2025-11-13 21:34:53 -0600

    about to console log but I need to figure out the proper path

--- src/utils/taxCalculator.ts
+++ src/utils/taxCalculator.ts
@@ -1,22 +1,15 @@
-import {Product} from "./models/Product.js";
+import {Product} from ".src/models/Product.js";
 
 
 // Inside src/utils/taxCalculator.ts, create a utility module to handle tax calculations.
 
 // utility function
 export function calculateTax(product: Product, taxDiscount: number = .15): number {
-
-    return `Product ${this.name} is $${this.getPriceWithTax()`;
+    const total = product.price + product.price * taxDiscount;
+    return total;
 }
 
 
-{
-   
-   
-   
-
-
-}
 
 
 // Add a function calculateTax() that accepts a Product object and returns the price including tax.
